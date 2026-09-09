209814f174de9befad8f0d18087115171c2fa722
kmcdotninja <yahayabinmuhammad@gmail.com>
2026-09-10 00:59:08 +0100

    chore: data changes

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     name: 'Kutuby',
     description:
-      'A learning app that turns Islamic studies for kids 6 to 12 into short, illustrated lessons that feel more like play than school.',
+      'A learning app that turns Islamic studies for kids, age 4 to 10 into short, illustrated lessons that feel more like play than school.',
     roles: ['Product Design', 'Art Direction', 'Website Design', 'Interaction Design'],
     team: [
       { name: 'Oselu Irabor', role: 'Illustrator' },
